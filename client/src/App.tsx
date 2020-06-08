ea4408589fff45d2f3f283283f1e0aea56545c35
Itay Hanya <itayy16@gmail.com>
2020-06-08 14:07:56 +0300

    Part 1a - show content in tickets

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -32,6 +32,7 @@ export class App extends React.PureComponent<{}, AppState> {
 		return (<ul className='tickets'>
 			{filteredTickets.map((ticket) => (<li key={ticket.id} className='ticket'>
 				<h5 className='title'>{ticket.title}</h5>
+				<span className='content'> {ticket.content} </span>
 				<footer>
 					<div className='meta-data'>By {ticket.userEmail} | { new Date(ticket.creationTime).toLocaleString()}</div>
 				</footer>
